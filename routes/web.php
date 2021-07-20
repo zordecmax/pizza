3082c261b7bd4119aa57ce1d7f803a8927afc3e8
Maxim <maximgrosul@gmail.com>
2021-07-20 19:37:53 +0300

    added validation on routes

--- routes/web.php
+++ routes/web.php
@@ -27,24 +27,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/search', function() {
-    $query = 'ita'; // <-- Change the query for testing.
-
-    $products = Product::search($query)->get();
-
-    return $products;
-});
+//Route::get('/search', function() {
+//    $query = 'ita'; // <-- Change the query for testing.
+//
+//    $products = Product::search($query)->get();
+//
+//    return $products;
+//});
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles');
-Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article');
-Route::get('/tag/{id}', [TagController::class, 'show'])->name('articlesByTag');
-Route::get('/article-category/{id}', [ArticleCategoryController::class, 'show'])->name('articlesByCategory');
+Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article')->whereNumber('id');
+Route::get('/tag/{id}', [TagController::class, 'show'])->name('articlesByTag')->whereNumber('id');
+Route::get('/article-category/{id}', [ArticleCategoryController::class, 'show'])
+    ->name('articlesByCategory')->whereNumber('id');
 
 Route::get('/orders/', [UserController::class, 'showOrders'])->name('userOrders')->middleware('auth');
 Route::get('/cabinet/', [UserController::class, 'showCabinet'])->name('userCabinet')->middleware('auth');
 
 
 Auth::routes();
+
 Route::get('/contacts', ['uses' => function () {
     return view('contacts');
 }, 'middleware' => \App\Http\Middleware\LoggingMiddleware::class]);
@@ -57,13 +59,13 @@ Route::view('/terms', 'terms')->name('terms');
 //Route::view('/contacts', 'contacts')->middleware(\App\Http\Middleware\LoggingMiddleware::class)->name('contacts');
 Route::post('/contacts', [ContactController::class, 'send'])->name('contacts');
 Route::get('/mail', [ContactController::class, 'send']);
-Route::get('/page/{slug}')->name('page');
+//Route::get('/page/{slug}')->name('page');
 Route::get('/menu', [ProductController::class, 'index'])->name('menu');
-Route::get('/menu/{slug}', [CategoryController::class, 'show'])->name('category');
-Route::get('/item/{id}', [ProductController::class, 'show'])->name('product');
+Route::get('/menu/{slug}', [CategoryController::class, 'show'])->name('category')->whereAlpha('slug');
+Route::get('/item/{id}', [ProductController::class, 'show'])->name('product')->whereNumber('id');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::post('/cart', [CartController::class, 'store'])->name('cart.add');
 Route::get('/cart/delete', [CartController::class, 'erase'])->name('cart.erase');
-Route::post('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/{id}', [CartController::class, 'destroy'])->name('cart.delete');
-Route::get('/cart/done', [OrderController::class, 'create'])->name('cartDelivery');
+//Route::post('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
+//Route::delete('/cart/{id}', [CartController::class, 'destroy'])->name('cart.delete');
+//Route::get('/cart/done', [OrderController::class, 'create'])->name('cartDelivery');
